342725423e98047a9e8f75f71c099c4acf701020
weronikasabiniewicz <weronikasabiniewicz@wp.pl>
2018-05-31 21:52:31 +0200

    Do routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,19 +10,12 @@ import { MessageService } from '../message.service'
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   
-  constructor(private heroService: HeroService, private messageService : MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void
-  {
-      this.selectedHero = hero;
-      this.messageService.add('Selected hero: ' + hero.name);
   }
 
   getHeroes(): void {
